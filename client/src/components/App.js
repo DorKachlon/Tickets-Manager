5021aeee9ecd0f8b1d68be6460337d328bfe0cc1
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-08-24 11:14:20 +0300

    Principals and gitignore

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import Ticket from "./Ticket";
+import "./App.css";
+
+function App() {
+    return <main></main>;
+}
+
+export default App;
