fa6983ad5f93ac3de86f0d650a8ad93ce7c64b3a
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-08-26 20:17:56 +0300

    After eslint fix

---
+++ client/src/naw.js
@@ -0,0 +1,29 @@
+// const puppeteer = require("puppeteer");
+// let browser;
+// let page;
+
+// beforeAll(async () => {
+//     // launch browser
+//     browser = await puppeteer.launch({
+//         headless: false, // headless mode set to false so browser opens up with visual feedback
+//         slowMo: 250, // how slow actions should be
+//     });
+//     // creates a new page in the opened browser
+//     page = await browser.newPage();
+//     page.emulate({
+//         viewport: {
+//             width: 500,
+//             height: 2400,
+//         },
+//         userAgent: "",
+//     });
+// });
+
+// describe("Nav tests", () => {
+//     test("done button works in our nav", async () => {
+//         await page.goto("http://localhost:3000/");
+//         await page.waitForSelector(".MuiButtonBase-root");
+//         await page.click(".MuiButtonBase-root");
+//         expect(1).toEq(1);
+//     });
+// });
