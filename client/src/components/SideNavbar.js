3b542dc2e0f7681606e1713df54d3d6220ca70dc
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-09-06 20:21:27 +0300

    hide req

--- client/src/components/SideNavbar.js
+++ client/src/components/SideNavbar.js
@@ -1,151 +1,155 @@
-import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import IconButton from '@material-ui/core/IconButton';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import SmsFailedIcon from '@material-ui/icons/SmsFailed';
-import AllInboxIcon from '@material-ui/icons/AllInbox';
-import DeleteIcon from '@material-ui/icons/Delete';
-import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
-import useStyles from '../useStyles';
+import React from "react";
+import { useTheme } from "@material-ui/core/styles";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import IconButton from "@material-ui/core/IconButton";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import SmsFailedIcon from "@material-ui/icons/SmsFailed";
+import AllInboxIcon from "@material-ui/icons/AllInbox";
+import DeleteIcon from "@material-ui/icons/Delete";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import useStyles from "../useStyles";
 
 export default function SideNavbar({
-  handleDrawerClose, open, valueOfNav, setValueOfNav,
+    handleDrawerClose,
+    open,
+    valueOfNav,
+    setValueOfNav,
 }) {
-  const classes = useStyles();
-  const theme = useTheme();
-  return (
-    <>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </div>
+    console.log("render side");
+    const classes = useStyles();
+    const theme = useTheme();
+    return (
+        <>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === "ltr" ? (
+                            <ChevronLeftIcon />
+                        ) : (
+                            <ChevronRightIcon />
+                        )}
+                    </IconButton>
+                </div>
 
-        <List>
-          <Divider />
-          <ListItem
-            button
-            key="All Tickets"
-            style={
-              valueOfNav === 1 || valueOfNav === 6
-                ? { backgroundColor: '#3F51B5', color: 'white' }
-                : {}
-            }
-            onClick={() => setValueOfNav(1)}
-          >
-            <ListItemIcon>
-              <AllInboxIcon
-                style={
-                  valueOfNav === 1 ? { color: 'white' } : {}
-                }
-              />
-            </ListItemIcon>
-            <ListItemText primary="All Tickets" />
-          </ListItem>
-          <Divider />
-          <ListItem
-            button
-            key="Done Tickets"
-            style={
-              valueOfNav === 2
-                ? { backgroundColor: '#3F51B5', color: 'white' }
-                : {}
-            }
-            onClick={() => setValueOfNav(2)}
-          >
-            <ListItemIcon>
-              <CheckCircleIcon
-                style={
-                  valueOfNav === 2 ? { color: 'white' } : {}
-                }
-              />
-            </ListItemIcon>
-            <ListItemText primary="Done Tickets" />
-          </ListItem>
-          <Divider />
-          <ListItem
-            button
-            key="Undone Tickets"
-            style={
-              valueOfNav === 3
-                ? { backgroundColor: '#3F51B5', color: 'white' }
-                : {}
-            }
-            onClick={() => setValueOfNav(3)}
-          >
-            <ListItemIcon>
-              <SmsFailedIcon
-                style={
-                  valueOfNav === 3 ? { color: 'white' } : {}
-                }
-              />
-            </ListItemIcon>
-            <ListItemText primary="Undone Tickets" />
-          </ListItem>
-          <Divider />
-          <ListItem
-            button
-            key="Hide Tickets"
-            style={
-              valueOfNav === 4
-                ? { backgroundColor: '#3F51B5', color: 'white' }
-                : {}
-            }
-            onClick={() => setValueOfNav(4)}
-          >
-            <ListItemIcon>
-              <VisibilityOffIcon
-                style={
-                  valueOfNav === 4 ? { color: 'white' } : {}
-                }
-              />
-            </ListItemIcon>
-            <ListItemText primary="Hide Tickets  NOTWORK" />
-          </ListItem>
-          <Divider />
-          <ListItem
-            button
-            key="Trash"
-            style={
-              valueOfNav === 5
-                ? { backgroundColor: '#3F51B5', color: 'white' }
-                : {}
-            }
-            onClick={() => setValueOfNav(5)}
-          >
-            <ListItemIcon>
-              <DeleteIcon
-                style={
-                  valueOfNav === 5 ? { color: 'white' } : {}
-                }
-              />
-            </ListItemIcon>
-            <ListItemText primary="Trash" />
-          </ListItem>
-          <Divider />
-        </List>
-      </Drawer>
-    </>
-  );
+                <List>
+                    <Divider />
+                    <ListItem
+                        button
+                        key="All Tickets"
+                        style={
+                            valueOfNav === 1 || valueOfNav === 6
+                                ? { backgroundColor: "#3F51B5", color: "white" }
+                                : {}
+                        }
+                        onClick={() => setValueOfNav(1)}
+                    >
+                        <ListItemIcon>
+                            <AllInboxIcon
+                                style={
+                                    valueOfNav === 1 ? { color: "white" } : {}
+                                }
+                            />
+                        </ListItemIcon>
+                        <ListItemText primary="All Tickets" />
+                    </ListItem>
+                    <Divider />
+                    <ListItem
+                        button
+                        key="Done Tickets"
+                        style={
+                            valueOfNav === 2
+                                ? { backgroundColor: "#3F51B5", color: "white" }
+                                : {}
+                        }
+                        onClick={() => setValueOfNav(2)}
+                    >
+                        <ListItemIcon>
+                            <CheckCircleIcon
+                                style={
+                                    valueOfNav === 2 ? { color: "white" } : {}
+                                }
+                            />
+                        </ListItemIcon>
+                        <ListItemText primary="Done Tickets" />
+                    </ListItem>
+                    <Divider />
+                    <ListItem
+                        button
+                        key="Undone Tickets"
+                        style={
+                            valueOfNav === 3
+                                ? { backgroundColor: "#3F51B5", color: "white" }
+                                : {}
+                        }
+                        onClick={() => setValueOfNav(3)}
+                    >
+                        <ListItemIcon>
+                            <SmsFailedIcon
+                                style={
+                                    valueOfNav === 3 ? { color: "white" } : {}
+                                }
+                            />
+                        </ListItemIcon>
+                        <ListItemText primary="Undone Tickets" />
+                    </ListItem>
+                    <Divider />
+                    <ListItem
+                        button
+                        key="Hide Tickets"
+                        style={
+                            valueOfNav === 4
+                                ? { backgroundColor: "#3F51B5", color: "white" }
+                                : {}
+                        }
+                        onClick={() => setValueOfNav(4)}
+                    >
+                        <ListItemIcon>
+                            <VisibilityOffIcon
+                                style={
+                                    valueOfNav === 4 ? { color: "white" } : {}
+                                }
+                            />
+                        </ListItemIcon>
+                        <ListItemText primary="Hide Tickets  NOTWORK" />
+                    </ListItem>
+                    <Divider />
+                    <ListItem
+                        button
+                        key="Trash"
+                        style={
+                            valueOfNav === 5
+                                ? { backgroundColor: "#3F51B5", color: "white" }
+                                : {}
+                        }
+                        onClick={() => setValueOfNav(5)}
+                    >
+                        <ListItemIcon>
+                            <DeleteIcon
+                                style={
+                                    valueOfNav === 5 ? { color: "white" } : {}
+                                }
+                            />
+                        </ListItemIcon>
+                        <ListItemText primary="Trash" />
+                    </ListItem>
+                    <Divider />
+                </List>
+            </Drawer>
+        </>
+    );
 }
--- server/app.js
+++ server/app.js
@@ -1,90 +1,117 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const fs = require('fs').promises;
+const fs = require("fs").promises;
 
-const filePath = './data.json';
+const filePath = "./data.json";
 
-app.use(express.static('../client/build'));
+app.use(express.static("../client/build"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // http://localhost:8080/api/tickets?searchText=Need+a+Little
 // http://localhost:8080/api/tickets?searchContent=my+code+to+display+commas
 // http://localhost:8080/api/tickets?Email=jug%40nesetal.af
-app.get('/api/tickets', async (req, res) => {
-  const content = await fs.readFile(filePath);
-  const arr = JSON.parse(content);
-  const { searchText } = req.query;
-  const { searchContent } = req.query;
-  const email = req.query.Email;
-  if (searchText) {
-    const filterArr = arr.filter((obj) => obj.title.toLowerCase().includes(searchText.toLowerCase()));
+app.get("/api/tickets", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const { searchText } = req.query;
+    const { searchContent } = req.query;
+    const email = req.query.Email;
+    if (searchText) {
+        const filterArr = arr.filter((obj) =>
+            obj.title.toLowerCase().includes(searchText.toLowerCase())
+        );
+        res.send(filterArr);
+    } else if (searchContent) {
+        const filterArr = arr.filter((obj) =>
+            obj.content.toLowerCase().includes(searchContent.toLowerCase())
+        );
+        res.send(filterArr);
+    } else if (email) {
+        const filterArr = arr.filter((obj) =>
+            obj.userEmail.toLowerCase().includes(email.toLowerCase())
+        );
+        res.send(filterArr);
+    } else {
+        res.send(arr);
+    }
+});
+
+app.get("/api/tickets/done", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const filterArr = arr.filter((obj) => obj.done);
     res.send(filterArr);
-  } else if (searchContent) {
-    const filterArr = arr.filter((obj) => obj.content.toLowerCase().includes(searchContent.toLowerCase()));
+});
+
+app.get("/api/tickets/undone", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const filterArr = arr.filter((obj) => !obj.done);
     res.send(filterArr);
-  } else if (email) {
-    const filterArr = arr.filter((obj) => obj.userEmail.toLowerCase().includes(email.toLowerCase()));
+});
+app.get("/api/tickets/deleted", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const filterArr = arr.filter((obj) => obj.delete);
     res.send(filterArr);
-  } else {
-    res.send(arr);
-  }
 });
 
-app.get('/api/tickets/done', async (req, res) => {
-  const content = await fs.readFile(filePath);
-  const arr = JSON.parse(content);
-  const filterArr = arr.filter((obj) => obj.done);
-  res.send(filterArr);
+app.post("/api/tickets/:ticketId/done", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
+    arr[index].done = true;
+    await fs.writeFile(filePath, JSON.stringify(arr));
+    res.send({ updated: true });
 });
 
-app.get('/api/tickets/undone', async (req, res) => {
-  const content = await fs.readFile(filePath);
-  const arr = JSON.parse(content);
-  const filterArr = arr.filter((obj) => !obj.done);
-  res.send(filterArr);
-});
-app.get('/api/tickets/deleted', async (req, res) => {
-  const content = await fs.readFile(filePath);
-  const arr = JSON.parse(content);
-  const filterArr = arr.filter((obj) => obj.delete);
-  res.send(filterArr);
+app.post("/api/tickets/:ticketId/undone", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
+    arr[index].done = false;
+    await fs.writeFile(filePath, JSON.stringify(arr));
+    res.send({ updated: true });
 });
 
-app.post('/api/tickets/:ticketId/done', async (req, res) => {
-  const content = await fs.readFile(filePath);
-  const arr = JSON.parse(content);
-  const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
-  arr[index].done = true;
-  await fs.writeFile(filePath, JSON.stringify(arr));
-  res.send({ updated: true });
+app.post("/api/tickets/:ticketId/delete", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
+    arr[index].delete = true;
+    await fs.writeFile(filePath, JSON.stringify(arr));
+    res.send({ updated: true });
+});
+app.post("/api/tickets/:ticketId/undelete", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
+    arr[index].delete = false;
+    await fs.writeFile(filePath, JSON.stringify(arr));
+    res.send({ updated: true });
 });
 
-app.post('/api/tickets/:ticketId/undone', async (req, res) => {
-  const content = await fs.readFile(filePath);
-  const arr = JSON.parse(content);
-  const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
-  arr[index].done = false;
-  await fs.writeFile(filePath, JSON.stringify(arr));
-  res.send({ updated: true });
+app.post("/api/tickets/:ticketId/hide", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
+    arr[index].hide = true;
+    await fs.writeFile(filePath, JSON.stringify(arr));
+    res.send({ updated: true });
 });
 
-app.post('/api/tickets/:ticketId/delete', async (req, res) => {
-  const content = await fs.readFile(filePath);
-  const arr = JSON.parse(content);
-  const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
-  arr[index].delete = true;
-  await fs.writeFile(filePath, JSON.stringify(arr));
-  res.send({ updated: true });
-});
-app.post('/api/tickets/:ticketId/undelete', async (req, res) => {
-  const content = await fs.readFile(filePath);
-  const arr = JSON.parse(content);
-  const index = arr.findIndex((obj) => obj.id === req.params.ticketId);
-  arr[index].delete = false;
-  await fs.writeFile(filePath, JSON.stringify(arr));
-  res.send({ updated: true });
+app.post("/api/tickets/UnHideAll", async (req, res) => {
+    const content = await fs.readFile(filePath);
+    const arr = JSON.parse(content);
+    for (let obj of arr) {
+        if (obj.hide === true) {
+            obj.hide = false;
+        }
+    }
+    await fs.writeFile(filePath, JSON.stringify(arr));
+    res.send({ updated: true });
 });
 
 module.exports = app;
