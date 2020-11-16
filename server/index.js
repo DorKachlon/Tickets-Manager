248b35714dac10ea26fc2d7102546ff95f9a5efd
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-11-16 16:42:20 +0200

    update port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 const app = require("./app");
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`)
-);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
